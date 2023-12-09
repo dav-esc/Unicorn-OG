cee2115bc4514fb924a760fce0ab0bae04745772
dav-esc <descobedo2001@gmail.com>
2023-12-08 22:25:58 -0600

    changes

--- js/ride.js
+++ js/ride.js
@@ -192,23 +192,17 @@ return Math.floor(Math.random() * (max - min + 1)) + min;
 // a function to show images and information of the breeds
 function showCatImageAndInformation(index) {
 
-// This will display the image of the cat
+//the image of the cat
   document.getElementById("cat_image").src = storedBreeds[index].image.url;
 
-// This will get the breed name
+// breed name
   document.getElementById("breed_name").innerHTML = storedBreeds[index].name;
 
-// This will get the wiki link
+//  wiki link
   document.getElementById("wiki_link").href = storedBreeds[index].wikipedia_url;
 
   document.getElementById("wiki_link").innerHTML =
     storedBreeds[index].wikipedia_url;  
-
-// This will get the characteristics of the cat
-  document.getElementById("breed_json").textContent =
-    storedBreeds[index].temperament;
-}
-
 
 // a function to retrieve data from the API
 fetch(API_url, {
@@ -224,7 +218,7 @@ fetch(API_url, {
     storedBreeds = data;
 
 
-    // Using the random function to select a specific breed. Then extracting information from that breed
+    // Using the random func to select a breed 
     showCatImageAndInformation(getRandomInt(0, storedBreeds.length - 1));
   })
   .catch(function (error) {
